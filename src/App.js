7921b5dba74b6d06a32543750a0360b4fb14020f
mrtsnvsk <nekit.255@yandex.com>
2021-01-10 04:07:43 +0300

    test dragNdrop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [cardList, setCardList] = useState([
+    { id: 1, order: 3, text: 'КАРТОЧКА 3' },
+    { id: 2, order: 1, text: 'КАРТОЧКА 1' },
+    { id: 3, order: 2, text: 'КАРТОЧКА 2' },
+    { id: 4, order: 4, text: 'КАРТОЧКА 4' },
+  ]);
+
+  const [currentCard, setCurrentCard] = useState(null);
+
+  const dragStartHandler = (e, card) => {
+    setCurrentCard(card);
+  };
+
+  const dragEndHandler = (e) => {
+    e.target.style.background = 'white';
+  };
+
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+    e.target.style.background = 'lightgray';
+  };
+
+  const dropHandler = (e, card) => {
+    e.preventDefault();
+    setCardList(
+      cardList.map((el) => {
+        if (el.id === card.id) {
+          return {
+            ...el,
+            order: currentCard.order,
+          };
+        }
+
+        if (el.id === currentCard.id) {
+          return {
+            ...el,
+            order: card.order,
+          };
+        }
+        return el;
+      })
+    );
+    e.target.style.background = 'white';
+  };
+
+  const sortCards = (a, b) => {
+    return a.order > b.order ? 1 : -1;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='app'>
+      {cardList.sort(sortCards).map((card) => (
+        <div
+          // чтобы эл-нт можно было перемещать
+          draggable={true}
+          // срабатывает в момент когда карточка взята
+          onDragStart={(e) => dragStartHandler(e, card)}
+          // если вышли за пределы другой карточки
+          onDragLeave={(e) => dragEndHandler(e)}
+          // если мы отпустили перемещение
+          onDragEnd={(e) => dragEndHandler(e)}
+          // если мы находимся над другим объектом
+          onDragOver={(e) => dragOverHandler(e)}
+          // отпустили карточку и ждем связанное с этим действие
+          onDrop={(e) => dropHandler(e, card)}
+          className='card'
+          key={card.id}
         >
-          Learn React
-        </a>
-      </header>
+          {card.text}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
